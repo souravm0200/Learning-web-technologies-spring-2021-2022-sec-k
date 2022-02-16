3779073827656bf7ff8f5ec71bfae06c0e1b299d
SOURAV MANDAL <sourav0200@gmail.com>
2022-02-16 21:00:20 +0600

    done

--- MID Lab-5.2 [session and cookie]/login.php
+++ MID Lab-5.2 [session and cookie]/login.php
@@ -1,29 +1,52 @@
-<?php
-
-  session_start();
-
-?>
 
 <html>
 <head>
-	<title>Login</title>
+	
+	<title>X Company</title>
 </head>
+
+<style>
+	.topright {
+  position: absolute;
+  top: 8px;
+  right: 100px;
+  font-size: 16px;
+}
+</style>
+
+<fieldset>
+
 <body>
-	<form method="POST" action="loginCheck.php">
-		<table>
+	<form method="POST" action="regCheck.php">
+          <h1>XCOMPANY</h1> 
+	        <div class="topright"><a href="home.php"> Home |</a> <a href="login.php"> Login |</a> <a href="registration.php">Registration </a></div>
+
+
+	   <fieldset style="width:500px">
+	   	<legend>LOGIN</legend>
+
+		     <table>
 			<tr>
 				<td>Username</td>
 				<td><input type="text" name="username" value=""></td>
 			</tr>
 			<tr>
 				<td>Password</td>
-				<td><input type="password" name="password" value=""></td>
+				<td><input type="password" name="password" value=""><hr></td>
 			</tr>
 			<tr>
 				<td></td>
 				<td><input type="submit" name="submit" value="Submit"></td>
 			</tr>
-		</table>
+		</table>                   
+              
+        
+		
+		
 	</form>
+	</fieldset>
+ 
 </body>
+</fieldset>
+
 </html>
